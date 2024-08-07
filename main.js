a1e8573e5a99066b65901df2602fbd37c17c1c27
malin <malin.rosen@medieinstitutet.se>
2024-08-07 10:41:30 +0200

    easier and better way to write functions + some css-changes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import "./src/css/style.css";
 import { acneStudiosList } from "./src/lists/acneStudiosList";
 
-//FIXA SÅ ATT OFFLINE/ONLINE FUNKAR ÄVEN INNE PÅ KATEGORIER
-
 function buildCategoryTree(categories, parentId = 'root') {
   return categories
     .filter(category => category.parent_category_id === parentId)
@@ -12,49 +10,10 @@ function buildCategoryTree(categories, parentId = 'root') {
     }));
 }
 
-const categories = acneStudiosList
-
-const categoryTree = buildCategoryTree(categories);
+const categoryTree = buildCategoryTree(acneStudiosList);
 console.log(categoryTree);
 
-
-function traverseTree(tree, callback) {
-  tree.forEach(node => {
-    callback(node);
-    if (node.categories && node.categories.length > 0) {
-      traverseTree(node.categories, callback);
-    }
-  });
-}
-
-// knappar för kategorierna
-const buttonContainer = document.getElementById("buttonContainer");
-
-categoryTree.forEach(rootCategory => {
-  const button = document.createElement('button');
-  button.innerHTML = rootCategory.id;
-  
-  button.addEventListener('click', () => {
-    displayCategoryList(rootCategory.categories);
-  });
-  buttonContainer.appendChild(button);
-});
-
-
-// Funktion för att visa en lista med kategorier
-function displayCategoryList(categories) {
-  const ul = document.getElementById("theList");
-  ul.innerHTML = '';
-  traverseTree(categories, (node) => {
-    const li = document.createElement("li");
-    li.innerHTML = node.id; 
-    ul.appendChild(li);
-  });
-}
-
-
 const ul = document.getElementById('theList')
-
 
 for (let i = 0; i < acneStudiosList.length; i++) {
   const li = document.createElement("li");
@@ -72,42 +31,127 @@ for (let i = 0; i < acneStudiosList.length; i++) {
 const onlineButton = document.getElementById('onlineButton');
 const offlineButton = document.getElementById('offlineButton');
 const showAllButton = document.getElementById('showAllButton');
-const onlineItems = document.querySelectorAll(".online");
-const offlineItems = document.querySelectorAll(".offline");
+
 
 onlineButton.innerHTML = "SHOW ONLINE PRODUCTS";
 offlineButton.innerHTML = "SHOW OFFLINE PRODUCTS";
 showAllButton.innerHTML = "SHOW ALL PRODUCTS"
 
-
-function showOnlineItems() {
-  onlineItems.forEach((item) => {
-    item.style.display = "block";
-  });
-  offlineItems.forEach((item) => {
-    item.style.display = "none";
-  });
-}
-
-function showOfflineItems() {
-  onlineItems.forEach((item) => {
-    item.style.display = "none";
-  });
-  offlineItems.forEach((item) => {
-    item.style.display = "block";
-  });
-}
-
-function showAllItems() {
-  onlineItems.forEach((item) => {
-    item.style.display = "block";
-  });
-  offlineItems.forEach((item) => {
-    item.style.display = "block";
-  });
-
-}
-
 onlineButton.addEventListener('click', showOnlineItems);
 offlineButton.addEventListener('click', showOfflineItems);
 showAllButton.addEventListener('click', showAllItems);
+
+
+
+const buttonContainer = document.getElementById("buttonContainer");
+
+categoryTree.forEach(rootCategory => {
+  const button = document.createElement('button');
+  button.innerHTML = rootCategory.id;
+  
+  button.addEventListener('click', () => {
+    displayCategoryList(rootCategory.categories);
+  });
+  buttonContainer.appendChild(button);
+});
+
+function traverseTree(tree, callback) {
+  tree.forEach(node => {
+    callback(node);
+    if (node.categories && node.categories.length > 0) {
+      traverseTree(node.categories, callback);
+    }
+  });
+}
+
+
+function updateItemVisibility() {
+  const allItems = document.querySelectorAll('.item'); 
+  allItems.forEach((item) => {
+    if (item.classList.contains('online')) {
+      item.style.display = showOnline ? "block" : "none";
+    } else if (item.classList.contains('offline')) {
+      item.style.display = showOffline ? "block" : "none";
+    }
+  });
+}
+
+
+let showOnline = true;
+let showOffline = true;
+
+
+function showOnlineItems() {
+  showOnline = true;
+  showOffline = false;
+  updateItemVisibility();
+}
+
+
+function showOfflineItems() {
+  showOnline = false;
+  showOffline = true;
+  updateItemVisibility();
+}
+
+
+function showAllItems() {
+  showOnline = true;
+  showOffline = true;
+  updateItemVisibility();
+}
+
+
+
+function displayCategoryList(categories) {
+  const ul = document.getElementById("theList");
+  ul.innerHTML = '';
+  traverseTree(categories, (node) => {
+    const li = document.createElement("li");
+    li.innerHTML = node.id; 
+    li.classList.add('item')
+    if (node.online) {
+      li.classList.add('online');
+    } else {
+      li.classList.add('offline');
+    }
+    ul.appendChild(li); 
+  });
+}
+
+
+
+
+// const onlineItems = document.querySelectorAll(".online");
+// const offlineItems = document.querySelectorAll(".offline");
+
+// function showOnlineItems() {
+//   onlineItems.forEach((item) => {
+//     item.style.display = "block";
+//   });
+//   offlineItems.forEach((item) => {
+//     item.style.display = "none";
+//   });
+// }
+
+// function showOfflineItems() {
+//   onlineItems.forEach((item) => {
+//     item.style.display = "none";
+//   });
+//   offlineItems.forEach((item) => {
+//     item.style.display = "block";
+//   });
+// }
+
+// function showAllItems() {
+//   onlineItems.forEach((item) => {
+//     item.style.display = "block";
+//   });
+//   offlineItems.forEach((item) => {
+//     item.style.display = "block";
+//   });
+
+// }
+
+
+
